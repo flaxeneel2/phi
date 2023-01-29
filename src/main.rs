26cec985e2957036891e3b13fc372afcde890da7
flaxeneel2 <me@flaxeneel2.net>
2023-01-29 11:55:13 +0000

    Added main class detection

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-use jni::{InitArgsBuilder, JavaVM, JNIVersion};
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use std::process::exit;
+use jni::{InitArgsBuilder, JavaVM, JNIEnv, JNIVersion};
 use jni::objects::JObject;
+use once_cell::sync::OnceCell;
+use serde_yaml::Value;
+use zip::ZipArchive;
 
 mod util;
 
+static JVM: OnceCell<JavaVM> = OnceCell::new();
+
 fn main() {
+    let main_class = get_main_class();
+    run_jar(main_class);
+}
+
+fn run_jar(main_class: String) {
     let jvm_args = InitArgsBuilder::new()
         .version(JNIVersion::V8)
         .option("-Xmx4G")
@@ -14,9 +28,53 @@ fn main() {
         .build()
         .unwrap();
     let jvm = JavaVM::new(jvm_args).unwrap();
-    let env = jvm.attach_current_thread().unwrap();
+    jvm.attach_current_thread_permanently().expect("Failed to bond with my own child!");
+    JVM.set(jvm).unwrap();
+    let env = jni();
     let arg_str = env.new_string("nogui").unwrap();
     let arg = env.new_object_array(1, "java/lang/String", arg_str).unwrap();
-    unsafe { env.call_static_method("io/papermc/paperclip/Paperclip", "main", "([Ljava/lang/String;)V", &[JObject::from_raw(arg).into()]).expect("ERR"); }
+    let jobj_arg = unsafe {
+        JObject::from_raw(arg)
+    };
+    env.call_static_method(main_class, "main", "([Ljava/lang/String;)V", &[jobj_arg.into()]).expect("ERR");
     log!("Loaded!");
+
+}
+
+
+fn get_main_class() -> String {
+    let mut reader = ZipArchive::new(File::open(Path::new("/home/flaxeneel2/Documents/projects/rust/phi/server/server.jar")).unwrap()).unwrap();
+    let mut meta = match reader.by_name("META-INF/MANIFEST.MF") {
+        Ok(meta) => {
+            meta
+        },
+        Err(err) => {
+            error!("Cannot read manifest! Error: {}", err);
+            exit(1)
+        }
+    };
+    let mut contents = "".to_string();
+    meta.read_to_string(&mut contents).unwrap();
+    let meta_parsed: Value = match serde_yaml::from_str(&*contents) {
+        Ok(meta_parsed) => {
+            meta_parsed
+        },
+        Err(err) => {
+            error!("Failed to parse meta! Error: {}", err);
+            exit(1)
+        }
+    };
+    match meta_parsed.get("Main-Class") {
+        Some(class) => {
+            class.as_str().unwrap().to_string().replace(".", "/")
+        },
+        None => {
+            error!("The meta doesnt contain a main class? what");
+            exit(1)
+        }
+    }
+}
+
+fn jni() -> JNIEnv<'static> {
+    JVM.get().unwrap().get_env().unwrap()
 }
